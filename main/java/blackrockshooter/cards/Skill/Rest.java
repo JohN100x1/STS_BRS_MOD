5b45fa190915c05af45eff78044d6f04aa2bd572
JohN100x1 <JohN100x1@hotmail.co.uk>
2022-08-22 11:04:44 +0100

    feat: update "Rest"

--- main/java/blackrockshooter/cards/Skill/Rest.java
+++ main/java/blackrockshooter/cards/Skill/Rest.java
@@ -1,27 +1,25 @@
 package blackrockshooter.cards.Skill;
 
-import blackrockshooter.actions.RemoveFromMasterDeckAction;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.HealAction;
-import com.megacrit.cardcrawl.actions.watcher.PressEndTurnButtonAction;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.localization.CardStrings;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import blackrockshooter.BlackRockShooterMod;
-import blackrockshooter.cards.AbstractDynamicCard;
+import blackrockshooter.cards.AbstractDefaultCard;
 import blackrockshooter.characters.BlackRockShooter;
 import blackrockshooter.powers.Sleep_pw;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.actions.unique.RemoveDebuffsAction;
+import com.megacrit.cardcrawl.actions.watcher.PressEndTurnButtonAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static blackrockshooter.BlackRockShooterMod.makeCardPath;
 
-public class Rest extends AbstractDynamicCard {
+public class Rest extends AbstractDefaultCard {
 
     /*
      * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
      *
-     * Retain. Heal 50%(all) of max HP. Gain 3 sleep. End your turn. Remove this card from your deck.
+     * Retain. Heal 15 of max HP. Remove all your Debuffs. Gain 3(2) sleep. End your turn. Exhaust.
      */
 
 
@@ -29,11 +27,6 @@ public class Rest extends AbstractDynamicCard {
 
     public static final String ID = BlackRockShooterMod.makeID(Rest.class.getSimpleName());
     public static final String IMG = makeCardPath("Rest.png");
-
-    // /TEXT DECLARATION/
-
-    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
 
 
     // STAT DECLARATION
@@ -45,6 +38,7 @@ public class Rest extends AbstractDynamicCard {
 
     private static final int COST = 3;
     private static final int MAGIC = 3;
+    private static final int UPGRADE_PLUS_MAGIC = -1;
 
 
     // /STAT DECLARATION/
@@ -53,22 +47,17 @@ public class Rest extends AbstractDynamicCard {
     public Rest() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         this.selfRetain = true;
-        this.purgeOnUse = true;
+        this.exhaust = true;
         baseMagicNumber = magicNumber = MAGIC;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        if (!upgraded) {
-            AbstractDungeon.actionManager.addToBottom(new HealAction(p, p, (int)(0.5F*p.maxHealth)));
-        }
-        else{
-            AbstractDungeon.actionManager.addToBottom(new HealAction(p, p, p.maxHealth));
-        }
+        AbstractDungeon.actionManager.addToBottom(new HealAction(p, p, 15));
+        AbstractDungeon.actionManager.addToBottom(new RemoveDebuffsAction(p));
         AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new Sleep_pw(p, p, magicNumber), magicNumber));
         AbstractDungeon.actionManager.addToBottom(new PressEndTurnButtonAction());
-        AbstractDungeon.actionManager.addToBottom(new RemoveFromMasterDeckAction(this));
     }
 
     //Upgraded stats.
@@ -76,7 +65,7 @@ public class Rest extends AbstractDynamicCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            rawDescription = UPGRADE_DESCRIPTION;
+            upgradeMagicNumber(UPGRADE_PLUS_MAGIC);
             initializeDescription();
         }
     }
